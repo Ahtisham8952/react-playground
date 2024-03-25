696c7938c97d73f3c918851f66e843babd2cfda3
Ahtisham8952 <ahtisham.badar786@gmail.com>
2024-03-26 01:39:27 +0500

    password generator

--- react-play/src/App.jsx
+++ react-play/src/App.jsx
@@ -1,34 +1,63 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useCallback, useEffect, useRef } from 'react'
+
+
 
 function App() {
-  const [count, setCount] = useState(5)
+
+
   
-const handleClick=()=>{
-  setCount(count+1)
-
-}
-const Addclick=()=>{
-  setCount(count-1)
-  if(count<=0){
-    alert("No more items left!")
-
-  }
-
-}
-
   return (
-    <>
     
-     
-    <div>
-     <h1>Count:{count}</h1>
-     <button onClick={handleClick}> Update Count</button>
-     <button onClick={Addclick}> Update Count</button>
+    <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500">
+      <h1 className='text-white text-center my-3'>Password generator</h1>
+    <div className="flex shadow rounded-lg overflow-hidden mb-4">
+        <input
+            type="text"
+          
+            className="outline-none w-full py-1 px-3"
+            placeholder="Password"
+            readOnly
+         
+        />
+        <button
+       
+        className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'
+        >copy</button>
+        
     </div>
-    </>
+    <div className='flex text-sm gap-x-2'>
+      <div className='flex items-center gap-x-1'>
+        <input 
+        type="range"
+        min={6}
+        max={100}
+
+         className='cursor-pointer'
+        
+          />
+          <label>Length: {length}</label>
+      </div>
+      <div className="flex items-center gap-x-1">
+      <input
+          type="checkbox"
+          
+          id="numberInput"
+         
+      />
+      <label htmlFor="numberInput">Numbers</label>
+      </div>
+      <div className="flex items-center gap-x-1">
+          <input
+              type="checkbox"
+             
+              id="characterInput"
+              
+          />
+          <label htmlFor="characterInput">Characters</label>
+      </div>
+    </div>
+</div>
+    
   )
 }
 
